e61425bb4b4a283fbd77e700a8af08e09a8599bd
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-24 21:13:10 -0300

    Feat: Criar Classe e adicionar método para pegar o nome e o tipo do animal

--- src/Pet.java
+++ src/Pet.java
@@ -1,2 +1,30 @@
 public class Pet {
+    private String name;
+    private String type;
+
+    public Pet(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{name='" + name + "', type='" + type + "'}";
+    }
+
 }
