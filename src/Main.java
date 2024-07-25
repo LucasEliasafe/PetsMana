af7022cc66561462df04a19bcf55e1e8a073dc92
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-25 20:37:07 -0300

    Feat:Crir classe Main para ser o menu para adicionar animais e listar eles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        PetManager petManager = new PetManager();
+
+        petManager.addPet("Rex", "Cachorro");
+        petManager.addPet("Mia", "Gato");
+    }
+}
