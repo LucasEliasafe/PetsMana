fb8298352a7891c85fb5c8f846cd625695522bf7
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-25 21:21:07 -0300

    Feat: Adicionar remover no menu o tipos de animais, adicionar no case 2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,40 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         PetManager petManager = new PetManager();
 
+        while (true) {
+            System.out.print("\nMenu");
+            System.out.println("1. Add pet");
+            System.out.println("2. Apagar Pet");
+            System.out.println("3. Listar pets");
+            System.out.println("4. Sair");
+            System.out.println("Escolha uma opção");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
 
-
-        System.out.println("Lista de pets");
-        petManager.listPets();
+            switch (opcao) {
+                case 1:
+                    System.out.print("Nome do Pet: ");
+                    String name = scanner.nextLine();
+                    System.out.print("Tipo do Pet: ");
+                    String type = scanner.nextLine();
+                    petManager.addPet(name, type);
+                break;
+                case 2:
+                    System.out.print("Nome do Pet a ser apagado: ");
+                    String nameToRemove = scanner.nextLine();
+                    petManager.removePet(nameToRemove);
+                    break;
+                case 3:
+                    System.out.print("Lista de pets: ");
+                    petManager.listPets();
+                    break;
+                case 4:
+                    System.out.println("Saindo");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
     }
 }
